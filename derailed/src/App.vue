ce4f67edd0e043a36a328afde60917f2822df786
flob.red <florensvb@gmail.com>
2019-04-25 08:55:13 +0200

    Setup DErAileD landing page

--- derailed/src/App.vue
+++ derailed/src/App.vue
@@ -2,32 +2,24 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>DErAileD</span>
+        <span class="font-weight-light"> ENOWARS DESIGN</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <LandingPage/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import LandingPage from './components/LandingPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage,
   },
   data () {
     return {
