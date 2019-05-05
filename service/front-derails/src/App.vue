c9b6bcc7cb5786b594fd2f10b4ee2a9ca5a1ebac
flob.red <florensvb@gmail.com>
2019-05-05 20:13:58 +0200

    add logout to frontend

--- service/front-derails/src/App.vue
+++ service/front-derails/src/App.vue
@@ -5,6 +5,10 @@
         <span>DErAileD</span>
         <span class="font-weight-light"> ENOWARS DESIGN</span>
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn @click="logout" flat>Logout</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
@@ -23,6 +27,12 @@ export default {
     return {
       //
     }
+  },
+  methods: {
+    logout() {
+        delete window.localStorage.token;
+        this.$router.push('/');
+    },
   }
 }
 </script>
