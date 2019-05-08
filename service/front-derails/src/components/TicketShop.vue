a4b13faa0c319a4849be6cb936747e6da7baa595
flob.red <florensvb@gmail.com>
2019-05-08 16:58:57 +0200

    add or remove tickets from users

--- service/front-derails/src/components/TicketShop.vue
+++ service/front-derails/src/components/TicketShop.vue
@@ -2,36 +2,67 @@
   <v-layout justify-space-between column>
     <div>
       <v-toolbar flat color="#424242">
-        <v-toolbar-title>Tickets out of here</v-toolbar-title>
+        <v-toolbar-title>Tickets that can get me out of here</v-toolbar-title>
       </v-toolbar>
       <v-data-table
         :headers="headers"
         :items="tickets"
         :loading="loading"
         class="elevation-1"
+        hide-actions
       >
         <template v-slot:items="props">
-          <td>{{ props.item.train }}</td>
-          <td>{{ require('moment')(props.item.departure).calendar() }}</td>
+          <td>{{ `${randomTrain()} ${props.item.train}` }}</td>
+          <td>{{ `🕓 ${require('moment')(props.item.departure).calendar()}` }}</td>
           <td>{{ props.item.train_type }}</td>
+          <td>
+            <v-btn
+              :disabled="alreadyOwns(props.item)"
+              :loading="loading"
+              @click="buyTicket(props.item)"
+              class="mr-2"
+              fab
+              flat
+              small
+            >
+              <v-icon small>
+                shopping_cart
+              </v-icon>
+            </v-btn>
+          </td>
         </template>
       </v-data-table>
     </div>
 
     <div>
       <v-toolbar flat color="#424242">
-        <v-toolbar-title>Tickets I own</v-toolbar-title>
+        <v-toolbar-title>Tickets I already own</v-toolbar-title>
       </v-toolbar>
       <v-data-table
         :headers="headers"
         :items="userTickets"
         :loading="loading"
         class="elevation-1"
+        hide-actions
       >
         <template v-slot:items="props">
-          <td>{{ props.item.train }}</td>
-          <td>{{ require('moment')(props.item.departure).calendar() }}</td>
+          <td>{{ `${randomTrain()} ${props.item.train}` }}</td>
+          <td>{{ `🕓 ${require('moment')(props.item.departure).calendar()}` }}</td>
           <td>{{ props.item.train_type }}</td>
+          <td>
+            <v-btn
+              :loading="loading"
+              @click="sellTicket(props.item)"
+              class="mr-2"
+              fab
+              flat
+              small
+            >
+              <v-icon small>
+                delete
+              </v-icon>
+            </v-btn>
+          </td>
         </template>
       </v-data-table>
     </div>
@@ -39,16 +70,20 @@
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
     data () {
         return {
             tickets: [],
             userTickets: [],
+            trains: ['🚞', '🚟', '🚂', '🚃', '🚄', '🚅', '🚆', '🚈', '🚉', '🚊', '🚋', '💺', '🚝', '🛤'],
             loading: true,
             headers: [
                 { text: 'Train', value: 'train' },
                 { text: 'Departure', value: 'departure' },
                 { text: 'Type', value: 'train_type' },
+                { text: 'Actions', value: 'name', sortable: false }
             ].map(header => {
                 return {
                     ...header,
@@ -62,7 +97,39 @@ export default {
         this.getUserTickets();
     },
     methods: {
-        async getTickets () {
+          randomTrain() {
+            return _.sample(this.trains);
+          },
+          alreadyOwns(ticket) {
+            return this.userTickets.map(ut => ut.id).includes(ticket.id);
+          },
+          async buyTicket(ticket) {
+          this.loading = true;
+          try {
+            await this.$axios.post('/add-ticket', {
+              ticket_id: ticket.id,
+            });
+
+            await this.getUserTickets();
+          } catch (e) {
+            // Snackbar?
+          }
+          this.loading = false;
+        },
+      async sellTicket(ticket) {
+          this.loading = true;
+          try {
+            await this.$axios.post('/remove-ticket', {
+              ticket_id: ticket.id,
+            });
+
+            await this.getUserTickets();
+          } catch (e) {
+            // Snackbar?
+          }
+          this.loading = false;
+        },
+        async getTickets() {
             this.loading = true;
             try {
                 const { data: tickets } = await this.$axios.get('/tickets');
