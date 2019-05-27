b51db598c89a91e604237affd437566171894070
nikola.atanasov <nikola.atanasov@valsight.com>
2019-05-27 20:35:23 +0200

    whops, forgot user profile page

---
+++ service/front-derails/src/components/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+
+    <router-link to="/ticket-shop" class="v-btn action-btn">Back</router-link>
+
+    <v-flex class="mb-4">
+      <v-avatar size="126" class="mr-4">
+       <img src="../../../../checker/1.jpg">
+      </v-avatar>
+      <v-btn @click="changeAvatar">Change Avatar</v-btn>
+    </v-flex>
+
+    <div>Username: </div>
+    <div>Role: user/premium/admin</div>
+    <div>Phone: </div>
+
+    <v-btn
+      class="btn btn-sm btn-secondary action-btn">
+      Update Profile Info
+    </v-btn>
+
+  </v-container>
+</template>
+
+
+
+<script>
+  export default{
+
+    methods: {
+          changeAvatar(){
+
+          },
+    }
+
+  }
+
+</script>
